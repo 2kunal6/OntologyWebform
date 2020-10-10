42a45c7984251776eb5f5b8ac6dd7bf26072fc72
Kunal Rout <2kunal6@gmail.com>
2020-10-10 13:56:57 +0200

    Removing comments

--- src/main/java/de/unibonn/UploadServlet.java
+++ src/main/java/de/unibonn/UploadServlet.java
@@ -26,24 +26,18 @@ public class UploadServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-
-        String description = request.getParameter("description");
-        String ontology_url = request.getParameter("ontology_url");
-
         OntologyProcessor ontologyProcessor = new OntologyProcessor();
         RDFConnector rdfConnectorQuery = new RDFConnector("isa_rdf_triples", "query");
+
+        String ontology_url = request.getParameter("ontology_url");
 
         InputStream fileContent = null;
         if(ontology_url==null || ontology_url.equals("")) {
             Part filePart = request.getPart("file");
-            //String fileName = Paths.get(filePart.getName()).getFileName().toString();
             fileContent = filePart.getInputStream();
-            //System.out.println(IOUtils.toString(fileContent));
         }
 
         RDFConnector rc = new RDFConnector("test", "query");
-        //Set<Node> classes = rc.getFusekiClasses();
-        //Set<Node> classes = rc.getClasses(fileContent);
         Set<OntClass> classes = rc.getClasses(fileContent, ontology_url);
 
         Map<OntClass, List<Restriction>> classRestrictions = ontologyProcessor.getClassRestrictions(classes);
@@ -61,7 +55,6 @@ public class UploadServlet extends HttpServlet {
         session.setAttribute("classes", classes);
 
         List<String> classList = classes.stream().map(s -> s.toString()).collect(Collectors.toList());
-        //List<String> classList = new ArrayList<String>();
 
         request.setAttribute("classList", classList);
         request.setAttribute("ontologyClasses", ontologyClasses);
